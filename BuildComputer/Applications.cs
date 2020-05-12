842ca70a895537e36ce90c4b0ea21e1b2c5e5ed7
Bill <william.hine2019@gmail.com>
2020-05-12 13:06:37 -0400

    added application class

---
+++ BuildComputer/Applications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildComputer
+{
+    public class Applications
+    {
+        public string applicationName;
+        public string applicationType;
+        public double requiredRam;
+        public double requiredStorage;
+
+        public Applications()
+        {
+            applicationName = "Slack";
+            applicationType = "Messaging Service";
+            requiredRam = 4.0; //in gigabytes
+            requiredStorage = 0.512; //in gigabytes
+        }
+    }
+}
